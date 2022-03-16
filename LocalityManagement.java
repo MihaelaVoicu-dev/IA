dfef6d3d790c619aea88a401b0e27449592a96e5
vMihaaaa <101694541+vMihaaaa@users.noreply.github.com>
2022-03-16 08:20:11 +0200

    Add files via upload

---
+++ LocalityManagement.java
@@ -0,0 +1,18 @@
+package mihaela.ia;
+
+public class LocalityManagement {
+
+	public static void main(String[] args)  {
+	// TODO Auto-generated method stub
+	
+	// l.ReadData();
+	 //    l.ShowTheLocalities();
+		
+	ListOfLocalities l=new ListOfLocalities();
+	
+l.ReadXml();
+	//	l.ShowTheLocalities();
+	}
+	
+	}
+
